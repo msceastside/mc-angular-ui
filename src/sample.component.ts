965f1b9c0a415b90505e7e3357ec8daba58fbe84
Michael Carpenter <msc.eastside@gmail.com>
2017-07-08 14:21:25 -0700

    Passing in CardModel

--- src/sample.component.ts
+++ src/sample.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserCardModel } from './user-card.model';
 
 @Component({
   selector: 'sample-component',
-  template: `<h1>Sample component</h1>`
+  template: `
+  <h1>Sample component</h1>
+  <div>{{userCard.title1}}</div>
+  <div>{{userCard.title2}}</div>
+  `
 })
 export class SampleComponent {
+  @Input() userCard: UserCardModel;
 
   constructor() {
   }
